17738fe7ff0f46b979c2b5badb71a3d7bd9593f8
Mihai Crisan <crisanmihai2004@gmail.com>
2026-07-13 00:31:30 +0300

    align theme toggle with the work page jump rail column on lg+ (right-4), keep right-6 padding on smaller screens

--- components/global-chrome.tsx
+++ components/global-chrome.tsx
@@ -131,7 +131,7 @@ export function GlobalChrome() {
 
       <motion.div
         animate={{ opacity: 1 }}
-        className="fixed right-6 bottom-[18px] z-50"
+        className="fixed right-6 bottom-[18px] z-50 lg:right-4"
         initial={{ opacity: 0 }}
         transition={{ duration: 0.6, delay: 0.3 }}
       >
